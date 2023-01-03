4f5d633101524b12bc8abb4849f28c933b618470
irfannisanci <irfan.nisanci@pia-team.com>
2023-01-03 19:21:43 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 100; i++) {
+            boolean isAsal = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isAsal = false;
+                    break;
+                }
+            }
+            if (isAsal)
+                System.out.print(i + ", ");
+    }
+}}
